5f497e21fa9ef784587b002f69413e76e31a4e20
Rajnarayan Dutta <rxdsrex@gmail.com>
2022-03-10 01:22:13 +0530

    Added constructor code for OSLCServer

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,45 @@
 import OSLCError from './OSLCError';
 import Namespaces from './namespaces';
+import RootServices from './RootServices';
+import ServiceProviderCatalog from './ServiceProviderCatalog';
+import ServiceProvider from './ServiceProvider';
 
+/**
+ * OSLCServer is he root of a JavaScript Node.js API for accessing OSLC resources.
+ * It provides a convenient JavaScript interface to OSLC REST services. This function
+ * constructs a generic OSLC server that can be used on any OSLC domain.
+ *
+ * @class
+ */
 class OSLCServer {
+  /** The URI of the OSLC server being accessed */
   serverURI: string;
 
+  /** The user name or authentication ID of the user */
   username: string;
 
+  /** The user's password credentials */
   password: string;
 
-  // rootservices: null;
+  /** The Jazz rootservices document */
+  rootServices: RootServices | undefined;
 
-  // serviceProviderCatalog: null;
+  /** The server's service provider catalog */
+  serviceProviderCatalog: ServiceProviderCatalog | undefined;
 
-  // serviceProviderTitle: null;
+  /** the project area name that user wants to access */
+  serviceProviderTitle: string | undefined;
 
-  // serviceProvider: null;
+  /** A service provider describing available services */
+  serviceProvider: ServiceProvider | undefined;
 
+  /**
+   * Construct a OSLCServer object
+   *
+   * @param serverURI - The server URI
+   * @param username - User name or authentication ID of the user
+   * @param password - User's password credentials
+   */
   constructor(serverURI: string, username: string, password: string) {
     this.serverURI = serverURI;
     this.username = username;
